29baf94a74145577baca95a79b947a488965bd5b
Gabriel Vieira <gabrielvra@outlook.com>
2023-02-28 21:01:39 -0300

    Create Pessoa.java

---
+++ bloco01/lista01/src/exercicio02/Pessoa.java
@@ -0,0 +1,13 @@
+package exercicio02;
+
+
+/**
+ * Escreva uma descrição da classe Pessoa aqui.
+ * 
+ * @author (seu nome) 
+ * @version (um número da versão ou uma data)
+ */
+public class Pessoa
+{
+    
+}
